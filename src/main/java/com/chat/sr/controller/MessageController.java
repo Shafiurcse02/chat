ecc6c4c1c1d351b7640b84b5aa9d084dd23e1801
Shafiurcse02 <Shafiurcse@gmail.com>
2025-08-19 17:08:06 +0600

    Initial commit

--- src/main/java/com/chat/sr/controller/MessageController.java
+++ src/main/java/com/chat/sr/controller/MessageController.java
@@ -1,8 +1,27 @@
 package com.chat.sr.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+
+import com.chat.sr.model.ChatMessage;
 
 @Controller
 public class MessageController {
+	
+	@MessageMapping("chat.addUser")
+	@SendTo("/topic/public")
+	public ChatMessage addUser(@Payload ChatMessage chatMessage,
+			SimpMessageHeaderAccessor headerAccessor) {
+		
+	}
+	
+	@MessageMapping("chat.sendMessage")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		
+	}
 
 }
