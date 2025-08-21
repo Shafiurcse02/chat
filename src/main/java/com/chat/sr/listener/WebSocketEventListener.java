0d50a2e6c8f7b92a0c6fd3dd650581f6b9698543
Shafiurcse02 <Shafiurcse@gmail.com>
2025-08-21 15:29:59 +0600

    Added Spring Security and JWT

--- src/main/java/com/chat/sr/listener/WebSocketEventListener.java
+++ src/main/java/com/chat/sr/listener/WebSocketEventListener.java
@@ -4,16 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import com.chat.sr.model.ChatMessage;
+import com.chat.sr.model.ChatMessage.MessageType;
 import com.chat.sr.service.UserService;
 
 @Component
 public class WebSocketEventListener {
+	
+	  @Autowired
+	    private SimpMessageSendingOperations messagingTemplate;  // একটাই যথেষ্ট
 	@Autowired
 	private UserService userService;
     private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
@@ -34,9 +39,12 @@ public class WebSocketEventListener {
         
         if (username != null) {
             logger.info("❌ User Disconnected: " + username);
+            System.out.println("❌ User Disconnected: " + username);
             userService.setUserOnlineStatus(username, false); 
             ChatMessage chatMessage=new ChatMessage();
-            chatMessage.setMsgtype(null);
+            chatMessage.setMsgtype(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
         }
     }
 
